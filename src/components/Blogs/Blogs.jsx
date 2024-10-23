1ffefad8b0749f440229dcfcd5caa51ade7e61f0
MAMUN0188 <msmamun305@gmail.com>
2024-10-23 11:04:35 +0600

    first commit

---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react';
+
+const Blogs = () => {
+    const [blogs, setBlogs] = useState([])
+    useEffect(() => {
+        fetch('blogs.json')
+        .then(response => response.json())
+        .then(data => setBlogs(data))
+    } , [])
+    
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Blogs;
